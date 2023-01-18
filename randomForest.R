61d676b724bd65fd5916ee126ff7eb0fde51bcc8
Haneesh <56464193+saihaneesh26@users.noreply.github.com>
2023-01-18 18:24:48 +0530

    Add files via upload

---
+++ randomForest.R
@@ -0,0 +1,26 @@
+#RANDOM FOREST
+
+library(randomForest)
+library(caTools)
+
+head(iris)
+
+split<-sample.split(iris,SplitRatio=0.7)
+
+train<- subset(iris,split==TRUE)
+test<-subset(iris,split==FALSE)
+
+input<-train[,1:4]
+target<-train[,5]
+model<-randomForest(input,target,ntree=500)
+
+pred <- predict(model,test)
+pred
+
+cm<-table(pred,test$Species)
+cm
+
+plot(model)
+varImpPlot(model)
+importance(model)
+summary(model)
